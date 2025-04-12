6263377bec888cf7c4c988b34ebbb8826eae947f
SantinoMaccio <smaccio@frba.utn.edu.ar>
2025-04-12 20:25:21 -0300

    Update hello.c

--- 00-ChelloWorld/hello.c
+++ 00-ChelloWorld/hello.c
@@ -1,1 +1,9 @@
+#include <stdio.h>
 
+int main(){
+  FILE *archivo = fopen("output.txt","w");
+  printf("Hola Juan Perez!\n");
+  fprintf(archivo, "HOLA SANTINO MACCIO!");
+  fclose(archivo);
+  return 0;
+}
